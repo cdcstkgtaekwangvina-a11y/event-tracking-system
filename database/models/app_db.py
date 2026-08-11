46bb5ee4ad356d253cda841bdfeedb2a3d726fc9
kienkent1 <xuongkienphung@gmail.com>
2026-08-11 11:56:55 +0700

    fix asyncpg cache

--- database/models/app_db.py
+++ database/models/app_db.py
@@ -25,7 +25,10 @@ else:
     engine = create_async_engine(
         db_url,
         poolclass=NullPool,
-        connect_args={"prepared_statement_cache_size": 0},
+        connect_args={
+            "statement_cache_size": 0,
+            "prepared_statement_cache_size": 0,
+        },
     )
 
 async_session_factory = async_sessionmaker(
