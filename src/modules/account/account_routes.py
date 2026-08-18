29aa52381a26fd53fd112cf248882e33fd0b933f
LeeJunKen <lekien120716@gmail.com>
2026-08-18 16:29:04 +0700

    merge account,user module

--- src/modules/account/account_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .account_apis import router as account_apis_router
-from .views.admin.account_admin_views import router as account_admin_views_router
-
-router = APIRouter()
-
-router.include_router(account_admin_views_router)
-router.include_router(account_apis_router)
